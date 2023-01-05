d7a1d617bab049887fde795ff087b15fff4cfc8a
jaewonjjang <wony3908@naver.com>
2023-01-05 18:27:35 +0900

    project1 가져옴

--- test.js
+++ test.js
@@ -1,32 +1,5 @@
-const todos =  [ {
-	'name' : '자바스크립트 공부하기', 
-	'tags' : ['programming', 'javascript'],
-	'status' : 'todo',
-	'id' : 1
-},
-{
-	'name' : ' 그림 그리기', 
-	'tags' : ['picture', 'favorite'],
-	'status' : 'doing',
-	'id' : 2
-},
-{
-	'name' : ' ios 공부하기', 
-	'tags' : ['ios', 'favorite'],
-	'status' : 'todo',
-	'id' : 3
-},
-{
-	'name' : ' 컴퓨터 수리하기', 
-	'tags' : ['angry', 'computer'],
-	'status' : 'done',
-	'id' : 4
-},
-{
-	'name' : ' 만화 보기', 
-	'tags' : ['cartoon'],
-	'status' : 'todo',
-	'id' : 5
-}
-]
-document.write(todos);
+var pi=5;
+var person=15;
+
+document.getElementById("total_number").innerHTML = 
+	pi + "<br>" + person;
