099aace54441e3727904468f46b2e3c53a9d13f5
SUNGWHANYOON <ysh0214727@naver.com>
2023-01-05 09:45:54 +0900

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const server = express();
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(3000, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port 3000");
+});
