0499fe3e2ffe5d2c7b9d93c562b4e903785d7d0c
Dongja <jdh981118@naver.com>
2023-01-05 16:41:33 +0900

    feat: add 누르면 아이템 생기고 한번 더 누르거나 포커스 아웃되면 삭제

--- sccript.js
+++ sccript.js
@@ -7,3 +7,48 @@ delete_btn.forEach(item=>{
             item.parentNode.parentNode.remove();
     })
 })
+
+const add_btn = document.querySelectorAll(".add");
+
+add_btn.forEach(item=>{
+    item.addEventListener('click',()=>{
+        const input_items = document.querySelector('.input-items');
+        // 만약 입력중인 상자가 있었다면 상자 지움
+        if(input_items)
+            input_items.remove();
+        else{
+            const child = item.parentNode.parentNode.parentNode.childNodes[3];
+        // console.log(child);
+        const lst_item = document.createElement("li");
+        const newForm = document.createElement('form');
+        const input_title = document.createElement('input');
+        input_title.setAttribute("type", "text");
+        input_title.setAttribute("placeholder", "제목을 입력하세요");
+	    const input_contents = document.createElement('input');
+        input_contents.setAttribute("placeholder", "내용을 입력하세요");
+
+        const delete_button = document.createElement("button");
+        delete_button.innerHTML = "취소";
+        const register_button = document.createElement("button");
+        register_button.innerHTML = "등록";
+
+        newForm.appendChild(input_title);
+        newForm.appendChild(input_contents);
+        newForm.appendChild(delete_button);
+        newForm.appendChild(register_button);
+
+        lst_item.appendChild(newForm);
+        lst_item.className = 'input-items';
+        lst_item.setAttribute("tabindex",'-1');
+        lst_item.addEventListener("blur", ()=>{
+            console.log(lst_item.parentNode);
+            console.log(lst_item);
+            // lst_item.parentNode.removeChild(lst_item);
+            lst_item.remove();
+        })
+        child.prepend(lst_item);
+
+        // console.log(lst_item);
+        }        
+    })
+})
