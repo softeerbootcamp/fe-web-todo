8ef5d73590452ed6faa749cea81d08293677db3b
SUMMERLOVE7 <wkdalswl77@naver.com>
2023-01-06 14:39:02 +0900

    Feat : 등록버튼 클릭시 칼럼 내에 컨텐츠 등록

--- src/inputContent.js
+++ src/inputContent.js
@@ -2,18 +2,18 @@ let addTodoContent = document.querySelector('#plus-todo-button');
 let closeTodoContent = document.querySelector('#x-todo-button');
 let todoTitleInput = document.querySelector('.todo-title-input');
 let todoCaptionInput = document.querySelector('.todo-caption-input');
-let openTodoModal = document.querySelector('#open-modal-todo');
+export let openTodoModal = document.querySelector('#open-modal-todo');
 
 let addDoingContent = document.querySelector('#plus-doing-button');
 let doingTitleInput = document.querySelector('.doing-title-input');
 let doingCaptionInput = document.querySelector('.doing-caption-input');
-let openDoingModal = document.querySelector('#open-modal-doing');
+export let openDoingModal = document.querySelector('#open-modal-doing');
 let closeDoingContent = document.querySelector('#x-doing-button');
 
 let addDoneContent = document.querySelector('#plus-done-button');
 let doneTitleInput = document.querySelector('.done-title-input');
 let doneCaptionInput = document.querySelector('.done-caption-input');
-let openDoneModal = document.querySelector('#open-modal-done');
+export let openDoneModal = document.querySelector('#open-modal-done');
 let closeDoneContent = document.querySelector('#x-done-button');
 
 function openModal(target){
@@ -27,7 +27,7 @@ addTodoContent.addEventListener('click', () => openModal(openTodoModal));
 addDoingContent.addEventListener('click', () => openModal(openDoingModal));
 addDoneContent.addEventListener('click', () => openModal(openDoneModal));
 
-function closeModal(target){
+export function closeModal(target){
     target.style.display = 'none';
     ///todoTitleInput.value = '';
     //todoCaptionInput.value = '';
--- src/registerContent.js
+++ src/registerContent.js
@@ -1,3 +1,5 @@
+import { closeModal, openTodoModal, openDoingModal, openDoneModal } from "./inputContent.js";
+
 let addTodoButton = document.querySelector('.todo-add-button');
 let contentTodo = document.querySelector('.havetodo-container');
 
@@ -8,7 +10,7 @@ let addDoneButton = document.querySelector('.done-add-button');
 let contentDone = document.querySelector('.done-container');
 
 let todolistToDo = document.querySelector('#todolist-todo');
-let listToDo = document.querySelector('#list-title-todo');
+let titleToDo = document.querySelector('#list-title-todo');
 let captionToDo = document.querySelector('#caption-todo');
 
 let newTodoTitle = document.querySelector('.todo-title-input');
@@ -20,6 +22,9 @@ let newDoingCaption = document.querySelector('.doing-caption-input');
 let newDoneTitle = document.querySelector('.done-title-input');
 let newDoneCaption = document.querySelector('.done-caption-input');
 
+//let todo_list = {};
+//let doing_list = [];
+//let done_list = [];
 
 function valid_input(target){
     if(!target.value){
@@ -33,6 +38,10 @@ function valid_input(target){
 
 function registerModal(target){
 
+    const newTitle = document.querySelector('#title-input').value;
+    const newContent = document.querySelector('#caption-input').value;
+    //todo_list[newTitle] = newContent;
+
     let newSection = document.createElement("section");
     let newClass = document.createAttribute("class");
     let newId = document.createAttribute("id");
@@ -44,12 +53,14 @@ function registerModal(target){
     let newClass2 = document.createAttribute("class");
     newClass2.value="list-title";
     newDiv.setAttributeNode(newClass2);
+    newDiv.innerText = newTitle;
     newSection.appendChild(newDiv);
 
     let newDiv2 = document.createElement("div");
     let newClass3 = document.createAttribute("class");
     newClass3.value = "caption";
     newDiv2.setAttributeNode(newClass3);
+    newDiv2.innerText = newContent;
     newSection.appendChild(newDiv2);
 
     target.appendChild(newSection);
@@ -59,24 +70,20 @@ function registerModal(target){
 addTodoButton.addEventListener('click', () => {
     if(valid_input(newTodoTitle) === 0){
         registerModal(contentTodo);
+        closeModal(openTodoModal);
     }
 });
-//addTodoButton.addEventListener('click', () => registerModal(contentTodo));
 
 addDoingButton.addEventListener('click', () => {
     if(valid_input(newDoingTitle) === 0){
         registerModal(contentDoing);
+        closeModal(openDoingModal);
     }
 });
-
-//addDoingButton.addEventListener('click', () => registerModal(contentDoing));
 
 addDoneButton.addEventListener('click', () => {
-    if(valid_input(newTodoTitle) === 0){
+    if(valid_input(newDoneTitle) === 0){
         registerModal(contentDone);
+        closeModal(openDoneModal);
     }
 });
-//addDoneButton.addEventListener('click', () => registerModal(contentDone));
-
-//const addbutton = document.getElementsByClassName('todo-add-button');
-//console.log(addbutton); 
