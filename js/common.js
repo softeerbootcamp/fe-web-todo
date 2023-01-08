d9491379b200883317131c16f0044f575f56ac0f
moon-GD <king4mun@gmail.com>
2023-01-08 13:48:10 +0900

    modify: 카드 column 배치 속성 inline-flex로 변경

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+// (target)의 css (key)를 (value)로 바꿈
 function changeCSS(target, key, value) {
     target.style[key] = value
 }
--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,6 @@ const menuBar = document.querySelector("#menu");
 const menuOpenBtn = document.querySelector("#menu-open-btn");
 const menuCloseBtn = document.querySelector("#menu-close-btn");
 
-
-console.log(menuOpenBtn)
+// menu toggle 이벤트 추가
 menuOpenBtn.addEventListener("click", () => { changeCSS(menuBar, "right", 0) })
 menuCloseBtn.addEventListener("click", () => { changeCSS(menuBar, "right", "-30vw") })
